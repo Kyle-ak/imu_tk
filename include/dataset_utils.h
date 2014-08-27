2e2e97ad4641edf863ee547b0971650a8afe61d6
Alberto Pretto <alberto.pretto@dei.unipd.it>
2014-08-27 13:38:25 +0200

    Starting inmplementation

--- include/dataset_utils.h
+++ include/dataset_utils.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <vector>
+
+namespace imu_calib
+{
+  void importMatlabData( std::vector< imu_calib::TriadData > &data );
+  void importMatlabData( std::vector< imu_calib::TriadData > &data0,
+                         std::vector< imu_calib::TriadData > &data1 );
+  void importMatlabData( std::vector< imu_calib::TriadData > &data0,
+                         std::vector< imu_calib::TriadData > &data1,
+                         std::vector< imu_calib::TriadData > &data12 );
+  
+}
